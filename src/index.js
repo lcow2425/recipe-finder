acd6ab606fe990b18ad2e79e7e9ad8502e25341c
c0932018 <c0932018@mylambton.ca>
2025-04-10 10:01:58 -0400

    Switch to HashRouter for GitHub Pages compatibility

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter } from 'react-router-dom'; // Use HashRouter instead of BrowserRouter
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
